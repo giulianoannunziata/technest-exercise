2f4491a8771633181c028070fe82ca2fad2c0437
giuliano <giuliano.annunziata@outlook.com>
2020-06-17 20:16:50 +0100

    Initial commit

---
+++ src/main/java/es/technest/technestexercise/TechnestExerciseApplication.java
@@ -0,0 +1,13 @@
+package es.technest.technestexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnestExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnestExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/technest/technestexercise/TechnestExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package es.technest.technestexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnestExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
